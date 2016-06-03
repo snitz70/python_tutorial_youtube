4e1cdb52b5254b0e81b988c3d169993b43d04cae
Brian Snyder <bsnyder@technology4retailers.com>
2016-06-03 14:45:27 -0500

    added method for add_entry

--- diary.py
+++ diary.py
@@ -1,22 +1,15 @@
 from collections import OrderedDict
 import datetime
+import sys
 
 from peewee import *
 
 db = SqliteDatabase('diary.db')
 
-menu = OrderedDict({
-    ('a', 'add_entry'),
-    ('v', 'view_entries'),  
-}
-
-)
-
 
 class Entry(Model):
     content = CharField()
     timestamp = DateTimeField(default=datetime.datetime.now)
-    # timestamp
 
     class Meta:
         database = db
@@ -30,19 +23,45 @@ def initialize():
 
 def menu_loop():
     """Show the menu"""
+    choice = None
+
+    while choice != 'q':
+        print("Enter 'q' to quit.")
+        for key, value in menu.items():
+            print('{}) {}'.format(key, value.__doc__))
+        choice = input('Action: ').lower().strip()
+
+        if choice in menu:
+            menu[choice]()
+
 
 
 def add_entry():
     """Add an Entry."""
+    print('Enter your entry.  Press Ctrl+d when finished.')
+    data = sys.stdin.read().strip()
+
+    if data:
+        if input('Save entry?  [Yn] ').lower() != 'n':
+            Entry.create(content=data)
+            print('Saved successfully!')
 
 
 def view_entries():
     """View previous entries."""
+    entries = Entry.select().order_by(Entry.timestamp.desc())
 
 
 def delete_entry(entry):
     """Delete an entry."""
 
+
+menu = OrderedDict([
+    ('a', add_entry),
+    ('v', view_entries),
+]
+
+)
 
 
 if __name__ == '__main__':
